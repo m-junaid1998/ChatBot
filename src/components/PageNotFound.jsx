c338eec2f9d150162e5a7c0acbf36c1897cda692
junaidirfan1 <junaidirfan1998@gmail.com>
2026-01-24 21:51:55 +0500

    add page not found page and update css

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import PagenotFound from "../assets/icons/error.png"
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import PagenotFoundImg from "../assets/icons/error.png";
 
 function PageNotFound() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <img src={PagenotFound} alt="PagenotFound"  width={100} height={100} />
+    <div className='pagenotfound'>
+      <img src={PagenotFoundImg} alt="Page Not Found" />
+      <h2>Oops! Page Not Found</h2>
+      <button 
+        onClick={() => navigate('/')}
+        className="pagenotfound-btn " 
+   
+      >
+        Go to Dashboard
+      </button>
     </div>
-  )
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
